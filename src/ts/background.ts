75a1aaeb40124e3612973ecbfb2dd0e9defa8f2b
John Promponas <g.prombonas@int.enartia.com>
2019-11-16 19:32:10 +0200

    Add new classes for user settings and local storage
    This commit encapsules all the local storage management functionality in a new
    class.
    
    Utilizes all the types defined in types.ts to ease development and debugging.
    
    Adds a new class that handles user settings and provides get methods for them.
    
    Changes the way badge text is displayed.
    
    Most errors now throw an Exception.
    
    Removes excessive console prints.

--- src/ts/background.ts
+++ src/ts/background.ts
@@ -1,20 +1,82 @@
 import { get as httpGet } from "http";
+import { BiasScoresMethods, BiasGoggles, ServiceResponse, Serializable, ExtRequestTypes, ExtRequest } from "./types"
+import { basename } from "path";
+import { log } from "util";
 
-enum BiasScores {
-    independentCascade = "ic",
-    linearThreshold = "lt",
-    pagerank = "pr"
+class UserSettings {
+    method: string;
+    goggles: string;
+    limit: number;
+    badgeColor: string;
+
+    private key = "userSettings";
+
+    constructor(method: BiasScoresMethods, goggles: BiasGoggles, limit: number, badgeColor: string) {
+        this.method = method;
+        this.goggles = goggles;
+        this.limit = limit;
+        this.badgeColor = badgeColor;
+    }
+
+    public getSettings(): string {
+        return JSON.stringify({
+            'method': this.method,
+            'goggles': this.goggles,
+            'limit': this.limit,
+            'badgeColor': this.badgeColor
+        });
+    }
+
+    public saveToLocalStorage(): void {
+        window.localStorage.setItem(this.key, this.getSettings());
+    }
+
 }
 
-enum BiasGoggles {
-    politicalParties = "political-parties",
-    footballTeams = "sport-teams"
+let userSetttings = new UserSettings(BiasScoresMethods.independentCascade,
+    BiasGoggles.politicalParties, 10, '#0000FF');
+
+userSetttings.saveToLocalStorage();
+
+class LocalStorage {
+
+    public static save(data: string) {
+
+        let serialized = this.serialize(data);
+
+        window.localStorage.setItem(serialized.key, JSON.stringify(serialized.value));
+    }
+
+    public static update(data: Serializable) {
+        window.localStorage.setItem(data.key, JSON.stringify(data.value));
+    }
+
+    public static delete(domain: string) {
+        window.localStorage.removeItem(domain);
+    }
+
+    public static get(domain: string): Serializable {
+        return {
+            key: domain,
+            value: JSON.parse(window.localStorage.getItem(domain))
+        };
+    }
+
+    private static serialize(data: string): Serializable {
+        let ret: ServiceResponse = JSON.parse(data);
+
+        return {
+            key: ret.doc.domain,
+            value: {
+                ic: ret.doc.ic,
+                lt: ret.doc.lt,
+                pr: ret.doc.pr,
+                limit: userSetttings.limit
+            }
+        };
+    }
+
 }
-
-let selectedScore = BiasScores.pagerank;            //default
-let selectedGoggles = BiasGoggles.politicalParties; //default
-let lifespan = 10; 
-
 function getDomainFromURL(target : string) : string{
     
     target = target.split("/")[2];
@@ -23,124 +85,111 @@ function getDomainFromURL(target : string) : string{
     return target;
 }
 
-function getRequestURL(target : string){
+function updateBadge(domain: string, method: string) {
+    //@ts-ignore
+    let score = Math.fround(parseFloat(LocalStorage.get(domain).value[method].bias_score) * 100);
+    let badgeText : string = "";
+    
+    if( score == 100 )
+        badgeText = '100';
+    else if( score < 1)
+        badgeText = score.toFixed(2);
+    else
+        badgeText = score.toFixed(1);
+    
+    console.log(score);
+
+    chrome.browserAction.setBadgeText({ text: badgeText });
+}
+
+function getRequestURL(target: string) {
 
     const prefix = 'http://139.91.183.23:3000/results?domain=';
 
-    const suffix = '&bc=' + selectedGoggles;
+    const suffix = '&bc=' + userSetttings.goggles;
 
-    //may be reduntant at this point
-    target = getDomainFromURL(target);
-
-    return prefix + target + suffix;
+    return prefix + encodeURIComponent(target) + suffix;
 }
 
-function queryService(activeTab : string) {
-    let data : any = '';
+function queryService(activeTab: string) {
+
+    let data: any = '';
     console.log('requesting: ' + activeTab);
 
-    httpGet(getRequestURL(activeTab), res => {
+    httpGet(getRequestURL(getDomainFromURL(activeTab)), res => {
 
-        console.log('Status code: ' + res.statusCode);
-    
-        res.on('data',chunk => {
+        res.on('data', chunk => {
             data += chunk;
         });
 
-        res.on('close', () =>{
-            data = JSON.parse(data);
-            //TODO: if data is not defined => error
-            let toStorage: Object = {
-                "ic" : data.doc.ic,
-                "lt" : data.doc.lt,
-                "pr" : data.doc.pr,
-                "life" : lifespan
-            };
+        res.on('close', () => {
+            if (res.statusCode !== 200)
+                throw new Error('HTTP Status code' + res.statusCode);
 
-            console.log(data.doc.domain);
-            console.log(toStorage);
-            console.log('end of request....');
-            
-            //@ts-ignore
-            chrome.browserAction.setBadgeText({text: (parseFloat(data.doc.pr.bias_score) * 100).toFixed(2)});
-            //@ts-ignore
-            let domain : string =  data.doc.domain;
-            window.localStorage.setItem(domain, JSON.stringify(toStorage));          
+            LocalStorage.save(data);
+            updateBadge(getDomainFromURL(activeTab), userSetttings.method);
         });
     });
 }
 
 
-function getBiasData(){
+function getBiasData() {
 
-    chrome.browserAction.setBadgeBackgroundColor({color: '#0000FF'});
+    chrome.browserAction.setBadgeBackgroundColor({ color: userSetttings.badgeColor });
 
-    chrome.tabs.query({'active' : true}, 
+    chrome.tabs.query({ 'active': true },
         tabs => {
 
-            let activeTab : string = '';
+            let activeTab: string = '';
 
-            if(typeof tabs[0].url === 'string' )
+            if (typeof tabs[0].url === 'string')
                 activeTab = tabs[0].url;
-            else{
+            else {
                 throw new Error('no-active-tab-found');
             }
 
-            let  domain = getDomainFromURL(activeTab);
-            let entry = window.localStorage.getItem(domain);
+            let localEntry = LocalStorage.get(getDomainFromURL(activeTab));
 
-            if( !entry ) {
+            if (!localEntry.value) {
                 console.log(activeTab + " not found.");
                 queryService(activeTab);
-            }else{
+            } else {
                 console.log(activeTab + " found.");
-                
-                entry = JSON.parse(entry);
-                //@ts-ignore
-                if( entry['life'] === 0 ){
-                    //data considered too old.
-                    window.localStorage.removeItem(domain);
-                    //new request
-                    queryService(activeTab);
-                }else{
-                    //@ts-ignore
-                    entry['life']--;
-                    window.localStorage.setItem(domain,JSON.stringify(entry));
-                }
 
-                //console.log(entry);
+                if (localEntry.value.limit === 0) {
+                    LocalStorage.delete(activeTab);
+                    queryService(activeTab);
+                } else {
+                    localEntry.value.limit--;
+                    LocalStorage.update(localEntry);
+                }
             }
-    });
+        });
 }
 
-try{
-    
-    chrome.runtime.onMessage.addListener(
-        (request, sender, sendRespone) => {
-            chrome.tabs.query({'active' : true}, 
-                (tabs) => {
-                    //@ts-ignore
-                    if( request.response_content === "bias-stats" ){
-                        //@ts-ignore
-                        let entry = window.localStorage.getItem(getDomainFromURL(tabs[0].url));
-                        sendRespone(entry);
-                    }
-                }
-            );
-            return true;
+function handleRequest(request: ExtRequest, sender: chrome.runtime.MessageSender, sendRespone: any) {
+
+    chrome.tabs.query({ 'active': true },
+        (tabs) => {
+
+            if (request.type === ExtRequestTypes.bias_stats) {
+                let localData = JSON.parse(window.localStorage.getItem(getDomainFromURL(tabs[0].url)));
+                sendRespone({ data: localData[userSetttings.method], method: userSetttings.method });
+            } else {
+                throw new Error('Uknown request type ' + request.type);
+            }
+
         }
     );
 
-    //@ts-ignore
-    chrome.webRequest.onCompleted.addListener(getBiasData, {urls: ["<all_urls>"] ,types: ["main_frame"]});
-}catch(e){
-    console.log(e);
+    return true;
 }
 
-window.localStorage.setItem("score", selectedScore);
-console.log('Default bias score is set to ' + selectedScore);
-
-window.localStorage.setItem("goggles", selectedGoggles);
-console.log('Default bias goggles are set to ' + selectedGoggles);
+try {
+    chrome.runtime.onMessage.addListener(handleRequest);
+    chrome.webRequest.onCompleted.addListener(getBiasData, { urls: ["<all_urls>"], types: ["main_frame"] });
+} catch (e) {
+    console.log(e);
+}
 
 console.log('installed.....');
