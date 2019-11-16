79ce399c9fb9eb8a596ed737f433b6a8cabe30a8
John Promponas <g.prombonas@int.enartia.com>
2019-11-16 19:30:50 +0200

    Change popup html

--- src/ts/popup.ts
+++ src/ts/popup.ts
@@ -2,5 +2,10 @@ chrome.runtime.sendMessage({response_content: "bias-stats"}, (response) => {
     console.log('received message');
     let div = document.createElement("div");
     div.innerText = response;
-    document.body.appendChild(div);
+    document.getElementById('method-' + response.method).setAttribute('checked','');
 });
+
+document.getElementById('form')?.addEventListener('submit', () => {
+    console.log('not yet implemented!');
+});
+
