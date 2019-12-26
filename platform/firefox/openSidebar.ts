133462af3236d0ce31e6b4f1bcbca261c31b4a29
John Promponas <g.prombonas@int.enartia.com>
2019-12-26 22:26:49 +0200

    Implement on/off of sidebar from toolbar
    Change firefox @types to use the ones from DefinitelyTyped.

--- platform/firefox/openSidebar.ts
+++ platform/firefox/openSidebar.ts
@@ -1,4 +1,28 @@
+import { contexBtn } from "../../src/ts/contextMenu";
+import { MessageType } from "../../src/ts/types";
 
-browser.browserAction.onClicked.addListener(() => {
+/* https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/User_actions */
+function open(){
     browser.sidebarAction.open();
+    browser.browserAction.onClicked.addListener(close);    
+}
+
+function close(){
+    browser.sidebarAction.close();
+    browser.browserAction.onClicked.addListener(open);
+}
+
+browser.browserAction.onClicked.addListener(close);
+/* */
+
+chrome.contextMenus.create(contexBtn);
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    switch (info.menuItemId) {
+        case contexBtn.id:
+            chrome.runtime.sendMessage({ type: MessageType.SHOW_DATA_FOR_LINK, data: info.linkUrl });
+            browser.sidebarAction.open();
+            console.log('sent event for ' + info.linkUrl);
+            break;
+    }
 });
