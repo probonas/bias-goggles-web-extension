17e5599eabcbb079caaa86194c1924085cda0eaf
John Promponas <g.prombonas@int.enartia.com>
2020-01-15 15:12:40 +0200

    Allow dublicate 404 messages and remove unused code

--- src/ts/uncrawled.ts
+++ src/ts/uncrawled.ts
@@ -2,25 +2,15 @@ export namespace uncrawled {
     const errorMsgId = 'error';
 
     export function create404Msg(url: string, cssClasses?: string[]) : HTMLElement {
-        if (errorMessageExists())
-            return;
 
         let err = document.createElement('p');
         err.id = errorMsgId;
-        err.innerText = 'We haven\'t crawled ' + url + ' yet!';
+        err.insertAdjacentHTML('beforeend','We haven\'t crawled ' + url + ' yet!')
 
         if (cssClasses !== undefined)
             err.classList.add(...cssClasses);
 
         return err;
-    }
-
-    export function errorMessageExists(): boolean {
-        return (document.getElementById(errorMsgId)) ? true : false;
-    }
-
-    export function removeCrawlErrorMessage() {
-        document.getElementById(errorMsgId).remove();
     }
 
 }
