ebabb40e986ae241cfc10e2566fbeac452e1fde9
Panagiotis Papadakos <papadako@ics.forth.gr>
2020-04-12 17:52:53 +0300

    chrome open using full viewport

--- platform/chromium/index.ts
+++ platform/chromium/index.ts
@@ -3,13 +3,13 @@ import { ContextBtnMsg } from "../../src/ts/types";
 
 chrome.browserAction.onClicked.addListener((tab) => {
     chrome.windows.create({
-        width: 550,
+        width: 576,
         height: 800,
         type: "popup",
         url: chrome.runtime.getURL("/popup.html")
     }, (window) => {
         setTimeout(() => {
             chrome.runtime.sendMessage({ updateWindowID: tab.windowId } as ContextBtnMsg);
-        },500);
+        }, 500);
     });
 });
