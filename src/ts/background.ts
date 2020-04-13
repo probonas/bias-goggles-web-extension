361b3d59c537d354b8d1906ddf4f3b049b6bca3d
Promponas Giannis <probonas_gianni13@hotmail.com>
2020-04-13 18:17:32 +0300

    Tidy background.js, fix logic

--- src/ts/background.ts
+++ src/ts/background.ts
@@ -2,20 +2,18 @@ import { userSettings } from "./usersettings";
 import { utils } from "./utils";
 import "./contextMenu";
 
-chrome.runtime.onInstalled.addListener(() => {
-    userSettings.initialize(() => {
-        utils.showCorrectBadge();
-        console.log('initialized user profile!');
-    });
-});
-
-chrome.runtime.onStartup.addListener(() => {
+/**
+ * When the extension starts, we have to show the correct badge,
+ * and setup the webRequest listeners.
+ */
+function initializeExtension () {
     utils.showCorrectBadge();
-
     userSettings.load(() => {
-
+        // add webRequest listener
         chrome.webRequest.onResponseStarted.addListener((details) => {
             userSettings.get((settings) => {
+                //retrieve data for all goggles user has installed, regarless
+                //of whether they are selected(i.e. opened in a tab) or not
                 settings.gogglesList.forEach(goggle => {
                     utils.getBiasDataForGoggles(utils.getDomainFromURL(details.url), goggle.id, () => { });
                 });
@@ -23,8 +21,25 @@ chrome.runtime.onStartup.addListener(() => {
         },
             { urls: ["<all_urls>"], types: ["main_frame"] }
         );
-
     });
+}
 
+/**
+ * Fires when extension is installed.
+ */
+chrome.runtime.onInstalled.addListener(() => {
+    userSettings.initialize(() => {
+        console.log('initialized user profile!');
+        initializeExtension();
+    });
+});
+
+/**
+ * Fires when browser opens.
+ * Doesn't fire upon installation!
+ */
+chrome.runtime.onStartup.addListener(() => {
+    console.log('started extension');
+    initializeExtension();
 });
 
